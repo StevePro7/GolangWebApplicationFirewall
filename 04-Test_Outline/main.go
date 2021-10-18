51a7af1b2b426469291649a2271c9b17a4521afa
StevePro7 <steveprozune@hotmail.com>
2021-10-18 11:51:16 +0100

    admin

--- 04-Test_Outline/main.go
+++ 04-Test_Outline/main.go
@@ -22,11 +22,35 @@ func HomeFunc(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func AdminFunc(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	resp := map[string]interface{} {"status": "smart reverse proxy hello!"}
+	out, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fprintf(w, string(out))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
 func main() {
 	fmt.Println("start")
 	bind := ":3080"
 	gmux := mux.NewRouter()
 	gmux.HandleFunc("/", HomeFunc).Methods("GET")
+
+	admin := mux.NewRouter()
+	admin.HandleFunc("/admin", AdminFunc).Methods("GET")
+	go func() {
+		if err := http.ListenAndServe(":3081", admin); err != nil {
+			log.Fatalf("unable to start admin server: %s", err.Error())
+		}
+	}()
+
 	fmt.Println("listen...")
 	log.Fatal(http.ListenAndServe(bind, gmux))
 }
