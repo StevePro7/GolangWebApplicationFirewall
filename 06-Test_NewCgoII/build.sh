39a369978b3eae3483b9dcab1fe38411cf58e5a0
StevePro7 <steveprozune@hotmail.com>
2021-10-18 15:07:44 +0100

    load rules

---
+++ 06-Test_NewCgoII/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+go clean -cache
+go run main.go
--- 06-Test_NewCgoII/modsec.c
+++ 06-Test_NewCgoII/modsec.c
@@ -7,7 +7,7 @@ RulesSet *rules = NULL;
 
 void MyCInit()
 {
-    printf("MyCInit start!\n");
+    printf("!MyCInit start!\n");
     if (modsec != NULL)
     {
         return;
@@ -17,9 +17,24 @@ void MyCInit()
     modsec = msc_init();
     //printf("msc_init() -end-\n");
 
-    printf("msc_create_rules_set() start\n");
+    //printf("msc_create_rules_set() start\n");
     rules = msc_create_rules_set();
-    printf("msc_create_rules_set() -end-\n");
+    //printf("msc_create_rules_set() -end-\n");
 
-    printf("MyCInit -end-!\n");
+    const char *error = NULL;
+    printf("msc_rules_add_file() start\n");
+    msc_rules_add_file(rules, "modsecdefault.conf", &error);
+    msc_rules_add_file(rules, "crs-setup.conf", &error);
+    msc_rules_add_file(rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error);
+    if (error != NULL)
+    {
+        printf("The Error '%s'\n", error);
+    }
+    //printf("msc_create_rules_set() start\n");
+    //rules = msc_create_rules_set();
+    //printf("msc_create_rules_set() -end-\n");
+
+    printf("msc_rules_add_file() -end-\n");
+    printf("!!MyCInit -end-!\n");
+    return;
 }
