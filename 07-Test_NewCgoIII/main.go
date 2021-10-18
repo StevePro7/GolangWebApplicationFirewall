e5c3d4c39b7822b86d1953c723890be8952e5f64
StevePro7 <steveprozune@hotmail.com>
2021-10-18 16:17:30 +0100

    upadte files

--- 07-Test_NewCgoIII/main.go
+++ 07-Test_NewCgoIII/main.go
@@ -6,10 +6,63 @@ package main
 import "C"
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"time"
 	"unsafe"
 )
+
+func HomeFunc(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data := "Home ModSec!!!!"
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		_, err := fmt.Fprintf(w, "%s", err.Error())
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+}
+
+func AdminFunc(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	resp := map[string]interface{}{"status": "smart reverse proxy hello!"}
+	out, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fprintf(w, string(out))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func TestFunc(w http.ResponseWriter, r *http.Request) {
+	log.Print("Processing request")
+	urlx, err := url.Parse("https://www.lightbase.io/freeforlife")
+	if err != nil {
+		log.Println(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(urlx)
+	director := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		director(req)
+		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+		req.Host = req.URL.Host
+		req.URL.Path = urlx.Path
+	}
+	proxy.ServeHTTP(w, r)
+}
 
 func InitModSec() {
 	//log.Println("initModSec start")
@@ -29,20 +82,66 @@ func modsec(url string) int {
 	return inter
 }
 
-func LimitMiddleware() {
-	//log.Println("LimitMiddleware start")
-	var url string
-	//url = "http://localhost:3080/test/artists.php"
-	url = "http://localhost:3080/test/artists.php?artist=0+div+1+union%23foo*%2F*bar%0D%0Aselect%23foo%0D%0A1%2C2%2Ccurrent_user"
+func LimitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("req.URL : \"%s\"", r.URL)
 
-	inter := modsec(url)
-	log.Println("Intervention??:", inter)
-	//log.Println("LimitMiddleware -end-")
+		urlx := r.URL.String()
+		inter := modsec(urlx)
+		//var inter int
+		//inter = 0
+		//if r.RequestURI == "/test/artists.php" {
+		//	inter = 1
+		//}
+
+		if inter > 0 {
+			log.Printf("==== Mod Security Blocked! ====")
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
+
+//func LimitMiddleware() {
+//	//log.Println("LimitMiddleware start")
+//	var url string
+//	//url = "http://localhost:3080/test/artists.php"
+//	url = "http://localhost:3080/test/artists.php?artist=0+div+1+union%23foo*%2F*bar%0D%0Aselect%23foo%0D%0A1%2C2%2Ccurrent_user"
+//
+//	inter := modsec(url)
+//	log.Println("Intervention??:", inter)
+//	//log.Println("LimitMiddleware -end-")
+//}
 
 func main() {
 	//log.Println("start")
-	InitModSec()
-	LimitMiddleware()
+	//InitModSec()
+	//LimitMiddleware()
 	//log.Println("-end-")
+
+	log.Println("testing")
+	bind := ":3080"
+	gmux := mux.NewRouter()
+	gmux.HandleFunc("/", HomeFunc).Methods("GET")
+	gmux.HandleFunc("/test/artists.php", TestFunc).Methods("GET")
+
+	admin := mux.NewRouter()
+	admin.HandleFunc("/admin", AdminFunc).Methods("GET")
+	go func() {
+		if err := http.ListenAndServe(":3081", admin); err != nil {
+			log.Fatalf("unable to start admin server: %s", err.Error())
+		}
+	}()
+
+	log.Printf("starting smart reverse proxy on [%s]", bind)
+
+	log.Printf("initialize mod sec")
+	InitModSec()
+
+	log.Printf("listening!")
+	if err := http.ListenAndServe(bind, LimitMiddleware(gmux)); err != nil {
+		log.Fatalf("unable to start web server: %s", err.Error())
+	}
 }
