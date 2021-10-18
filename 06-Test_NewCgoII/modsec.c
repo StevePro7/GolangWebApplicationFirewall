a195be4e570acdbc5c825fd95ba83f7b84b27019
StevePro7 <steveprozune@hotmail.com>
2021-10-18 15:33:13 +0100

    intervention

--- 06-Test_NewCgoII/modsec.c
+++ 06-Test_NewCgoII/modsec.c
@@ -2,6 +2,7 @@
 #include "modsecurity/modsecurity.h"
 #include "modsecurity/rules_set.h"
 #include "modsecurity/transaction.h"
+#include "modsecurity/intervention.h"
 
 ModSecurity *modsec = NULL;
 RulesSet *rules = NULL;
@@ -38,12 +39,21 @@ int MyCProcess(const char *uri)
     Transaction *transaction = NULL;
     transaction = msc_new_transaction(modsec, rules, NULL);
     msc_process_connection(transaction, "127.0.0.1", 80, "127.0.0.1", 80);
-    fprintf(stderr, "URI='%s'", uri);
+    fprintf(stderr, "URI='%s'\n", uri);
 
     msc_process_uri(transaction, uri, "CONNECT", "1.1");
     msc_process_request_headers(transaction);
     msc_process_request_body(transaction);
 
-    printf("MyCProcess -end- [%s]\n", uri);
-    return 6;
+    ModSecurityIntervention intervention;
+    intervention.status = 200;
+    intervention.url = NULL;
+    intervention.log = NULL;
+    intervention.disruptive = 0;
+
+    int inter = msc_intervention(transaction, &intervention);
+    fprintf(stderr, "intervention=%i\n", inter);
+
+    printf("MyCProcess -end-...!!! [%s]\n", uri);
+    return inter;
 }
