ce37b5c2723ea0c13fae184bf3011805437b6390
StevePro7 <steveprozune@hotmail.com>
2021-10-18 14:59:20 +0100

    rules set

--- 06-Test_NewCgoII/modsec.c
+++ 06-Test_NewCgoII/modsec.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "modsecurity/modsecurity.h"
+#include "modsecurity/rules_set.h"
 
 ModSecurity *modsec = NULL;
+RulesSet *rules = NULL;
 
 void MyCInit()
 {
@@ -11,9 +13,13 @@ void MyCInit()
         return;
     }
 
-    printf("msc_init() start\n");
+    //printf("msc_init() start\n");
     modsec = msc_init();
-    printf("msc_init() -end-\n");
+    //printf("msc_init() -end-\n");
+
+    printf("msc_create_rules_set() start\n");
+    rules = msc_create_rules_set();
+    printf("msc_create_rules_set() -end-\n");
 
     printf("MyCInit -end-!\n");
 }
