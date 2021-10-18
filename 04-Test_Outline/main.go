d7b4e7dae06758edfc253a259a36494d77ac15a5
StevePro7 <steveprozune@hotmail.com>
2021-10-18 12:05:35 +0100

    test

--- 04-Test_Outline/main.go
+++ 04-Test_Outline/main.go
@@ -6,12 +6,14 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func HomeFunc(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	data := "Home ModSec!"
+	data := "Home ModSec!!!!"
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		_, err := fmt.Fprintf(w, "%s", err.Error())
@@ -37,11 +39,30 @@ func AdminFunc(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func TestFunc(w http.ResponseWriter, r *http.Request) {
+	log.Print("testHandler")
+	urlx, err := url.Parse("http://www.lightbase.io/freeforlife")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Print(urlx)
+	proxy := httputil.NewSingleHostReverseProxy(urlx)
+	director := proxy.Director
+	proxy.Director = func(req * http.Request) {
+		director(req)
+		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+		req.Host = req.URL.Host
+		req.URL.Path = urlx.Path
+	}
+	proxy.ServeHTTP(w, r)
+}
+
 func main() {
-	fmt.Println("start")
+	log.Println("testing")
 	bind := ":3080"
 	gmux := mux.NewRouter()
 	gmux.HandleFunc("/", HomeFunc).Methods("GET")
+	gmux.HandleFunc("/test", TestFunc).Methods("GET")
 
 	admin := mux.NewRouter()
 	admin.HandleFunc("/admin", AdminFunc).Methods("GET")
@@ -51,6 +72,8 @@ func main() {
 		}
 	}()
 
-	fmt.Println("listen...")
-	log.Fatal(http.ListenAndServe(bind, gmux))
+	log.Printf("starting smart reverse proxy on [%s]", bind)
+	if err := http.ListenAndServe(bind, gmux); err != nil {
+		log.Fatalf("unable to start web server: %s", err.Error())
+	}
 }
