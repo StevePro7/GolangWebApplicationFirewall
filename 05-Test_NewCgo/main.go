abd224153316c6617d8c2abff6eab2118486fb18
StevePro7 <steveprozune@hotmail.com>
2021-10-18 14:27:01 +0100

    multiple files

--- 05-Test_NewCgo/main.go
+++ 05-Test_NewCgo/main.go
@@ -4,19 +4,8 @@ import (
 	"log"
 )
 
-// #cgo CPPFLAGS: -I/usr/local/modsecurity/include
-// #cgo LDFLAGS: /usr/local/modsecurity/lib/libmodsecurity.so
-// #include "modsec.c"
-import "C"
-
-func initModSec() {
-	log.Println("initModSec! start")
-	C.MyCInit()
-	log.Println("initModSec! -end-")
-}
-
 func main() {
 	log.Println("start")
-	initModSec()
+	InitModSec()
 	log.Println("-end-")
 }
---
+++ 05-Test_NewCgo/modsec.go
@@ -0,0 +1,14 @@
+package main
+
+// #cgo CPPFLAGS: -I/usr/local/modsecurity/include
+// #cgo LDFLAGS: /usr/local/modsecurity/lib/libmodsecurity.so
+// #include "modsec.c"
+import "C"
+
+import "log"
+
+func InitModSec() {
+	log.Println("!initModSec! start")
+	C.MyCInit()
+	log.Println("!initModSec! -end-")
+}
