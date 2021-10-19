0bcd82296f2c525e4c0ded6b6abcdd51e9ec3bb6
StevePro7 <steveprozune@hotmail.com>
2021-10-19 16:48:01 +0100

    deny

--- 07-Test_NewCgoIII/modsec.c
+++ 07-Test_NewCgoIII/modsec.c
@@ -22,17 +22,37 @@ void MyCInit()
     msc_rules_add_file(rules, "modsecdefault.conf", &error);
     msc_rules_add_file(rules, "crs-setup.conf", &error);
     msc_rules_add_file(rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error);
-
     if (error != NULL)
     {
         printf("msc_rules_add_file error : '%s'\n", error);
     }
 
+    //msc_rules_dump(rules);
     //printf("MyCInit -end-\n");
     return;
 }
 
-int MyCProcess(char *uri)
+int MyCProcess(char *uri, char *body)
+{
+    Transaction *transaction = NULL;
+    transaction = msc_new_transaction(modsec, rules, NULL);
+    msc_process_connection(transaction, "127.0.0.1", 80, "127.0.0.1", 80);
+    fprintf(stderr, "URI=%s\n", uri);
+    msc_process_uri(transaction, uri, "CONNECT", "1.1");
+    msc_process_request_headers(transaction);
+    msc_process_request_body(transaction);
+    ModSecurityIntervention intervention;
+    intervention.status = 200;
+    intervention.url = NULL;
+    intervention.log = NULL;
+    intervention.disruptive = 0;
+    int inter = msc_intervention(transaction, &intervention);
+    fprintf(stderr, "intervention=%i\n", inter);
+    return inter;
+}
+
+/*
+int MyCProcess(char *uri, char *body)
 {
     printf("MyCProcess start [%s]\n", uri);
     int inter = 0;
@@ -41,27 +61,85 @@ int MyCProcess(char *uri)
     msc_process_connection(transaction, "127.0.0.1", 80, "127.0.0.1", 80);
     fprintf(stderr, "URI='%s'\n", uri);
 
-    //msc_process_uri(transaction, uri, "CONNECT", "1.1");
-    msc_process_uri(transaction, uri, "CONNECT", "2.0");
-    msc_process_request_headers(transaction);
-    msc_process_request_body(transaction);
-    //msc_process_response_headers(transaction);
-    //emsc_process_response_body(transaction);
-
     ModSecurityIntervention intervention;
     intervention.status = 200;
-    intervention.url = NULL;
+    intervention.url = uri;
     intervention.log = NULL;
     intervention.disruptive = 0;
 
+    printf("intervention URL [%s]\n", intervention.url);
+
+    printf("... msc_intervention START\n");
     inter = msc_intervention(transaction, &intervention);
+    printf("... msc_intervention -END-\n");
     fprintf(stderr, "intervention=%d\n", inter);
 
-    printf("val1 %d\n", intervention.status);
-    printf("val2 %s\n", intervention.url);
-    printf("val3 %s\n", intervention.log);
-    printf("val4 %d\n", intervention.disruptive);
 
-    printf("MyCProcess -end-...!!! [%s]\n", uri);
+    msc_process_uri(transaction, uri, "GET", "1.1");
+    printf("...connect\n");
+    //msc_process_uri(transaction, uri, "CONNECT", "2.0");
+
+//    printf("... msc_intervention START\n");
+//    inter = msc_intervention(transaction, &intervention);
+//    printf("... msc_intervention -END-\n");
+//    fprintf(stderr, "intervention=%d\n", inter);
+
+
+//    msc_append_request_body(transaction, body, 10);
+//    printf("...msc_append_request_body ['%s']\n", body);
+//    printf("...\n");
+//
+//    printf("... msc_intervention START\n");
+//    inter = msc_intervention(transaction, &intervention);
+//    printf("... msc_intervention -END-\n");
+//    fprintf(stderr, "intervention=%d\n", inter);
+
+
+    msc_process_request_headers(transaction);
+    printf("...msc_process_request_headers\n");
+    printf("...\n");
+
+    printf("... msc_intervention START\n");
+    inter = msc_intervention(transaction, &intervention);
+    printf("... msc_intervention -END-\n");
+    fprintf(stderr, "intervention=%d\n", inter);
+
+
+    printf("...msc_process_request_body start\n");
+    msc_process_request_body(transaction);
+    printf("...msc_process_request_body -end-\n");
+    printf("...\n");
+
+    printf("... msc_intervention START\n");
+    inter = msc_intervention(transaction, &intervention);
+    printf("... msc_intervention -END-\n");
+    fprintf(stderr, "intervention=%d\n", inter);
+
+
+    printf("...msc_process_response_headers -end-\n");
+    printf("...\n");
+    msc_process_response_headers(transaction, 200, "HTTP 1.1");
+
+    printf("...msc_process_response_body -end-\n");
+    msc_process_response_body(transaction);
+
+    printf("...msc_process_logging -end-\n");
+    msc_process_logging(transaction);    //msc_process_response_headers(transaction);
+    //emsc_process_response_body(transaction);
+
+
+
+    printf("... msc_intervention START\n");
+    inter = msc_intervention(transaction, &intervention);
+    printf("... msc_intervention -END-\n");
+    fprintf(stderr, "intervention=%d\n", inter);
+
+//    printf("val1 %d\n", intervention.status);
+//    printf("val2 %s\n", intervention.url);
+//    printf("val3 %s\n", intervention.log);
+//    printf("val4 %d\n", intervention.disruptive);
+
+    printf("MyCProcess -end-!! [%s]\n", uri);
     return inter;
 }
+*/
