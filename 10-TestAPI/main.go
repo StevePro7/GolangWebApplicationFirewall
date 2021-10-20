204c619f01e3f766d68090954adfaeeaf7cdf6ab
StevePro7 <steveprozune@hotmail.com>
2021-10-20 11:23:30 +0100

    new func

--- 10-TestAPI/main.go
+++ 10-TestAPI/main.go
@@ -8,11 +8,11 @@ import (
 	"net/http"
 )
 
-func HelloFunc(w http.ResponseWriter, _ *http.Request) {
-	log.Println("API Hello -start")
+func HomeFunc(w http.ResponseWriter, _ *http.Request) {
+	log.Println("API Home -start")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	data := "API Hello function"
+	data := "API Home function"
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		_, err := fmt.Fprintf(w, "%s", err.Error())
@@ -20,14 +20,30 @@ func HelloFunc(w http.ResponseWriter, _ *http.Request) {
 			log.Fatalln(err)
 		}
 	}
-	log.Println("API Hello --end-")
+	log.Println("API Home --end-")
+}
+
+func TestFunc(w http.ResponseWriter, _ *http.Request) {
+	log.Println("API Test -start")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data := "API Test function"
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		_, err := fmt.Fprintf(w, "%s", err.Error())
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	log.Println("API Test --end-")
 }
 
 func main() {
 	log.Println("API start")
 	bind := ":3080"
 	gmux := mux.NewRouter()
-	gmux.HandleFunc("/", HelloFunc).Methods("GET")
+	gmux.HandleFunc("/", HomeFunc).Methods("GET")
+	gmux.HandleFunc("/test/artists.php", TestFunc).Methods("GET")
 	log.Printf("API listen on [%s]\n", bind)
 	log.Fatal(http.ListenAndServe(bind, gmux))
 }
