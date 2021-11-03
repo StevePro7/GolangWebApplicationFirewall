179659837b64790d8b277cc5d480b9340ab6e1b4
StevePro7 <steveprozune@hotmail.com>
2021-11-03 16:39:36 +0000

    backend server

--- RestApi/20-EnvoyAuthzRepeat/backend_server/http_server.go
+++ RestApi/20-EnvoyAuthzRepeat/backend_server/http_server.go
@@ -2,54 +2,35 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/http2"
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"golang.org/x/net/http2"
 )
 
-//var ()
+var ()
 
 func fronthandler(w http.ResponseWriter, r *http.Request) {
-
 	log.Println("/ called")
-	fmt.Println("/ called")
-
 	requestDump, err := httputil.DumpRequest(r, true)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(err)
 	}
-
-	log.Println(string(requestDump))
 	fmt.Println(string(requestDump))
-
 	w.Header().Set("X-Custom-Header-From-Backend", "from backend")
-	log.Println(w, "ok")
-	_, err = fmt.Fprint(w, "ok")
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
+	fmt.Fprint(w, "ok")
 }
+
 func main() {
+
 	http.HandleFunc("/", fronthandler)
 
 	var server *http.Server
 	server = &http.Server{
 		Addr: ":11000",
 	}
-	err := http2.ConfigureServer(server, &http2.Server{})
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-
-	fmt.Println("Starting backend server...")
-	log.Println("Starting backend server...")
-
-	err = server.ListenAndServe()
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
+	http2.ConfigureServer(server, &http2.Server{})
+	log.Println("Starting Server..")
+	server.ListenAndServe()
 }
