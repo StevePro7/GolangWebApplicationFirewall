12cb0dd3911e27eff9884d5c6767f6a941f72962
StevePro7 <steveprozune@hotmail.com>
2021-11-04 19:11:42 +0000

    init cgo

--- RestApi/30-EnvoyAuthzInt00/authz_server/modsec.c
+++ RestApi/30-EnvoyAuthzInt00/authz_server/modsec.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include "modsecurity/modsecurity.h"
+#include "modsecurity/rules_set.h"
+#include "modsecurity/transaction.h"
+#include "modsecurity/intervention.h"
+
+ModSecurity *modsec = NULL;
+RulesSet *rules = NULL;
 
 void MyCInit()
 {
-    printf("  inside MyCInit()\n");
+    printf("MyCInit start\n");
+    //printf("MyCInit fileName='%s'\n", file_name);
+    if (modsec != NULL)
+    {
+        return;
+    }
+
+    modsec = msc_init();
+    rules = msc_create_rules_set();
+
+    const char *error = NULL;
+    //msc_rules_add_file(rules, file_name, &error);
+    msc_rules_add_file(rules, "rules/crs-setup.conf", &error);
+    msc_rules_add_file(rules, "rules/modsecdefault.conf", &error);
+    msc_rules_add_file(rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error);
+    if (error != NULL)
+    {
+        printf("msc_rules_add_file error : '%s'\n", error);
+    }
+
+    msc_rules_dump(rules);
+    printf("MyCInit -end-\n");
+    return;
 }
