d3149edd5be568e69d57f14915061e5cedc34360
StevePro7 <steveprozune@hotmail.com>
2021-11-04 19:51:11 +0000

    integrate modsec

--- RestApi/30-EnvoyAuthzInt00/authz_server/grpc_server.go
+++ RestApi/30-EnvoyAuthzInt00/authz_server/grpc_server.go
@@ -11,7 +11,10 @@ import (
 	"github.com/gogo/googleapis/google/rpc"
 	"log"
 	"net"
+	"strconv"
 	"strings"
+	"time"
+	"unsafe"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -58,18 +61,59 @@ func (a *AuthorizationServer) Check(_ context.Context, req *auth.CheckRequest) (
 	}
 
 	myhttp := req.GetAttributes().GetRequest().GetHttp()
-	log.Println(">>> GetId() >>  ", myhttp.GetId())
-	log.Println(">>> GetMethod() >>  ", myhttp.GetMethod())
-	log.Println(">>> GetHeaders() >>  ", myhttp.GetHeaders())
-	log.Println(">>> GetPath() >>  ", myhttp.GetPath())
-	log.Println(">>> GetHost() >>  ", myhttp.GetHost())
-	log.Println(">>> GetScheme() >>  ", myhttp.GetScheme())
-	log.Println(">>> GetQuery() >>  ", myhttp.GetQuery())
-	log.Println(">>> GetFragment() >>  ", myhttp.GetFragment())
-	log.Println(">>> GetSize() >>  ", myhttp.GetSize())
-	log.Println(">>> GetProtocol() >>  ", myhttp.GetProtocol())
-	log.Println(">>> GetBody() >>  ", myhttp.GetBody())
-	log.Println(">>> GetRawBody() >>  ", myhttp.GetRawBody())
+	//log.Println(">>> GetId() >>  ", myhttp.GetId())
+	//log.Println(">>> GetMethod() >>  ", myhttp.GetMethod())
+	//log.Println(">>> GetHeaders() >>  ", myhttp.GetHeaders())
+	//log.Println(">>> GetPath() >>  ", myhttp.GetPath())
+	//log.Println(">>> GetHost() >>  ", myhttp.GetHost())
+	//log.Println(">>> GetScheme() >>  ", myhttp.GetScheme())
+	//log.Println(">>> GetQuery() >>  ", myhttp.GetQuery())
+	//log.Println(">>> GetFragment() >>  ", myhttp.GetFragment())
+	//log.Println(">>> GetSize() >>  ", myhttp.GetSize())
+	//log.Println(">>> GetProtocol() >>  ", myhttp.GetProtocol())
+	//log.Println(">>> GetBody() >>  ", myhttp.GetBody())
+	//log.Println(">>> GetRawBody() >>  ", myhttp.GetRawBody())
+
+	// TODO - is the same as previous
+	// e.g.
+	// /test/artists.php?artist=0+div+1+union%23foo*%2F*bar%0D%0Aselect%23foo%0D%0A1%2C2%2Ccurrent_user
+
+	uri := myhttp.GetPath()
+	log.Println(">>> url() >>  ", uri)
+	httpMethod := myhttp.GetMethod()
+	log.Println(">>> GetMethod() >>  ", httpMethod)
+
+	tempProtocol := myhttp.GetProtocol()
+	httpProtocol, httpVersion := split(tempProtocol, "/")
+	log.Println(">>> 01() >>  ", httpProtocol)
+	log.Println(">>> 02() >>  ", httpVersion)
+
+	clientSocket := myhttp.GetHost()
+	clientLink, tempPort := split(clientSocket, ":")
+	clientPort, _ := strconv.Atoi(tempPort)
+	log.Println(">>> 01() >>  ", clientLink)
+	log.Println(">>> 02() >>  ", clientPort)
+
+	serverSocket := myhttp.GetHost()
+	serverLink, dataPort := split(serverSocket, ":")
+	serverPort, _ := strconv.Atoi(dataPort)
+	log.Println(">>> 01() >>  ", serverLink)
+	log.Println(">>> 02() >>  ", serverPort)
+
+	inter := modsec(uri, httpMethod, httpProtocol, httpVersion, clientLink, clientPort, serverLink, serverPort)
+	if inter > 0 {
+		log.Printf("==== Mod Security Blocked! ====")
+		//http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+
+		// TODO - why can't get this as before
+		// e.g.
+		// https://github.com/StevePro7/GolangWebApplicationFirewall/blob/main/RestApi/10-EnvoyAuthz/authz_server/grpc_server.go
+		return &auth.CheckResponse{
+			Status: &rpcstatus.Status{
+				Code: int32(rpc.PERMISSION_DENIED),
+			},
+		}, nil
+	}
 
 	log.Println("Auth Server Check() method -end-")
 	return &auth.CheckResponse{
@@ -91,10 +135,42 @@ func (a *AuthorizationServer) Check(_ context.Context, req *auth.CheckRequest) (
 	}, nil
 }
 
+func split(input, delim string) (left, right string) {
+	split := strings.SplitN(input, delim, 2)
+	left = split[0]
+	right = split[1]
+	return left, right
+}
 func InitModSec() {
 	log.Println("initModSec start")
 	C.MyCInit()
 	log.Println("initModSec -end-")
+}
+
+func modsec(url, httpMethod, httpProtocol, httpVersion string, clientLink string, clientPort int, serverLink string, serverPort int) int {
+	log.Println("modsec start ", url)
+	Curi := C.CString(url)
+	ChttpMethod := C.CString(httpMethod)
+	ChttpProtocol := C.CString(httpProtocol)
+	ChttpVersion := C.CString(httpVersion)
+	CclientLink := C.CString(clientLink)
+	CclientPort := C.int(clientPort)
+	CserverLink := C.CString(serverLink)
+	CserverPort := C.int(serverPort)
+
+	defer C.free(unsafe.Pointer(Curi))
+	defer C.free(unsafe.Pointer(ChttpMethod))
+	defer C.free(unsafe.Pointer(ChttpProtocol))
+	defer C.free(unsafe.Pointer(ChttpVersion))
+	defer C.free(unsafe.Pointer(CclientLink))
+	defer C.free(unsafe.Pointer(CserverLink))
+
+	start := time.Now()
+	inter := int(C.MyCProcess(Curi, ChttpMethod, ChttpProtocol, ChttpVersion, CclientLink, CclientPort, CserverLink, CserverPort))
+	elapsed := time.Since(start)
+	log.Printf("modsec()=%d, elapsed: %s", inter, elapsed)
+	log.Println("modsec -end-")
+	return inter
 }
 
 func main() {
--- RestApi/30-EnvoyAuthzInt00/authz_server/modsec.c
+++ RestApi/30-EnvoyAuthzInt00/authz_server/modsec.c
@@ -33,3 +33,31 @@ void MyCInit()
     printf("MyCInit -end-\n");
     return;
 }
+
+int MyCProcess(char *uri, char *http_method, char *http_protocol, char *http_version, char *client_link, int client_port, char *server_link, int server_port)
+{
+    Transaction *transaction = NULL;
+    transaction = msc_new_transaction(modsec, rules, NULL);
+
+    fprintf(stderr, "URI='%s'\n", uri);
+    fprintf(stderr, "Method='%s'\n", http_method);
+    fprintf(stderr, "Protocol='%s/%s'\n", http_protocol, http_version);
+    fprintf(stderr, "Client socket='%s:%d'\n", client_link, client_port);
+    fprintf(stderr, "Server socket='%s:%d'\n", server_link, server_port);
+
+    msc_process_connection(transaction, client_link, client_port, server_link, server_port);
+    msc_process_uri(transaction, uri, http_protocol, http_version);
+    msc_process_request_headers(transaction);
+    msc_process_request_body(transaction);
+
+    ModSecurityIntervention intervention;
+    intervention.status = 200;
+    intervention.url = NULL;
+    intervention.log = NULL;
+    intervention.disruptive = 0;
+    int inter = msc_intervention(transaction, &intervention);
+
+    fprintf(stderr, "intervention=%i\n", inter);
+    fprintf(stderr, "inter status=%i\n", intervention.status);
+    return inter;
+}
