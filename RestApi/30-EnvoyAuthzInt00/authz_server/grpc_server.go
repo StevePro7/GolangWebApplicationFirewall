eae1710e125abba17d2ba8c35f8668bacebc17d5
StevePro7 <steveprozune@hotmail.com>
2021-11-04 17:46:16 +0000

    project 30

---
+++ RestApi/30-EnvoyAuthzInt00/authz_server/grpc_server.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/gogo/googleapis/google/rpc"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	_ "google.golang.org/grpc/codes"
+	_ "google.golang.org/grpc/status"
+
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	rpcstatus "google.golang.org/genproto/googleapis/rpc/status"
+)
+
+var (
+	grpcport = flag.String("grpcport", ":50051", "grpcport")
+)
+
+type AuthorizationServer struct{}
+
+func (a *AuthorizationServer) Check(_ context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
+	log.Println("Auth Server Check() method start")
+
+	b, err := json.MarshalIndent(req.Attributes.Request.Http.Headers, "", "  ")
+	if err == nil {
+		log.Println("Inbound Headers: ")
+		log.Println(string(b))
+	}
+
+	ct, err := json.MarshalIndent(req.Attributes.ContextExtensions, "", "  ")
+	if err == nil {
+		log.Println("Context Extensions: ")
+		log.Println(string(ct))
+	}
+
+	authHeader, ok := req.Attributes.Request.Http.Headers["authorization"]
+	var splitToken []string
+
+	if ok {
+		splitToken = strings.Split(authHeader, "Bearer ")
+	}
+
+	log.Println("Auth header= " + authHeader)
+
+	if len(splitToken) == 2 {
+		log.Println("Split token: " + splitToken[1])
+	}
+
+	log.Println("Auth Server Check() method -end-")
+	return &auth.CheckResponse{
+		Status: &rpcstatus.Status{
+			Code: int32(rpc.OK),
+		},
+		HttpResponse: &auth.CheckResponse_OkResponse{
+			OkResponse: &auth.OkHttpResponse{
+				Headers: []*core.HeaderValueOption{
+					{
+						Header: &core.HeaderValue{
+							Key:   "x-custom-header-from-authz",
+							Value: "some value",
+						},
+					},
+				},
+			},
+		},
+	}, nil
+}
+
+func main() {
+
+	flag.Parse()
+
+	if *grpcport == "" {
+		fmt.Fprintln(os.Stderr, "missing -grpcport flag (:50051)")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	lis, err := net.Listen("tcp", *grpcport)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{grpc.MaxConcurrentStreams(10)}
+	opts = append(opts)
+
+	s := grpc.NewServer(opts...)
+	auth.RegisterAuthorizationServer(s, &AuthorizationServer{})
+
+	log.Printf("Starting gRPC Server at %s", *grpcport)
+	s.Serve(lis)
+
+}
---
+++ RestApi/30-EnvoyAuthzInt00/backend_server/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20211104170005-ce137452f963
+
+require golang.org/x/text v0.3.6 // indirect
