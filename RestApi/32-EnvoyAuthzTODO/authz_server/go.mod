507bceab99735dcf2126ea2be0a492cbbaf87a9f
StevePro7 <steveprozune@hotmail.com>
2021-11-05 09:51:13 +0000

    Init project 32

---
+++ RestApi/32-EnvoyAuthzTODO/authz_server/go.mod
@@ -0,0 +1,21 @@
+module main
+
+go 1.17
+
+require (
+	github.com/envoyproxy/go-control-plane v0.10.0
+	github.com/gogo/googleapis v1.4.1
+	golang.org/x/net v0.0.0-20211104170005-ce137452f963
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247
+	google.golang.org/grpc v1.42.0
+)
+
+require (
+	github.com/cncf/xds/go v0.0.0-20211011173535-cb28da3451f1 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ RestApi/32-EnvoyAuthzTODO/authz_server/grpc_server.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	rpcstatus "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/health"
+
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+
+	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type/v3"
+	"github.com/gogo/googleapis/google/rpc"
+)
+
+var (
+	grpcport = flag.String("grpcport", ":50051", "grpcport")
+	conn     *grpc.ClientConn
+	hs       *health.Server
+)
+
+type AuthorizationServer struct{}
+
+func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
+	log.Println(">>> Authorization called check()")
+
+	b, err := json.MarshalIndent(req.Attributes.Request.Http.Headers, "", "  ")
+	if err == nil {
+		log.Println("Inbound Headers: ")
+		log.Println((string(b)))
+	}
+
+	ct, err := json.MarshalIndent(req.Attributes.ContextExtensions, "", "  ")
+	if err == nil {
+		log.Println("Context Extensions: ")
+		log.Println((string(ct)))
+	}
+
+	authHeader, ok := req.Attributes.Request.Http.Headers["authorization"]
+	var splitToken []string
+
+	if ok {
+		splitToken = strings.Split(authHeader, "Bearer ")
+	}
+	if len(splitToken) == 2 {
+		token := splitToken[1]
+
+		if token == "foo" {
+			return &auth.CheckResponse{
+				Status: &rpcstatus.Status{
+					Code: int32(rpc.OK),
+				},
+				HttpResponse: &auth.CheckResponse_OkResponse{
+					OkResponse: &auth.OkHttpResponse{
+						Headers: []*core.HeaderValueOption{
+							{
+								Header: &core.HeaderValue{
+									Key:   "x-custom-header-from-authz",
+									Value: "some value",
+								},
+							},
+						},
+					},
+				},
+			}, nil
+		} else {
+			return &auth.CheckResponse{
+				Status: &rpcstatus.Status{
+					Code: int32(rpc.PERMISSION_DENIED),
+				},
+				HttpResponse: &auth.CheckResponse_DeniedResponse{
+					DeniedResponse: &auth.DeniedHttpResponse{
+						Status: &envoy_type.HttpStatus{
+							Code: envoy_type.StatusCode_Unauthorized,
+						},
+						Body: "PERMISSION_DENIED",
+					},
+				},
+			}, nil
+
+		}
+
+	}
+	return &auth.CheckResponse{
+		Status: &rpcstatus.Status{
+			Code: int32(rpc.UNAUTHENTICATED),
+		},
+		HttpResponse: &auth.CheckResponse_DeniedResponse{
+			DeniedResponse: &auth.DeniedHttpResponse{
+				Status: &envoy_type.HttpStatus{
+					Code: envoy_type.StatusCode_Unauthorized,
+				},
+				Body: "Authorization Header malformed or not provided",
+			},
+		},
+	}, nil
+}
+
+func main() {
+
+	flag.Parse()
+
+	if *grpcport == "" {
+		fmt.Fprintln(os.Stderr, "missing -grpcport flag (:50051)")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	lis, err := net.Listen("tcp", *grpcport)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{grpc.MaxConcurrentStreams(10)}
+	opts = append(opts)
+
+	s := grpc.NewServer(opts...)
+
+	auth.RegisterAuthorizationServer(s, &AuthorizationServer{})
+
+	log.Printf("Starting gRPC Server at %s", *grpcport)
+	s.Serve(lis)
+
+}
