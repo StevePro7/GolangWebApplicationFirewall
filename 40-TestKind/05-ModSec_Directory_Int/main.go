5437e6041cdb362e4d47cf1ffe5b1201cfb3bf15
StevePro7 <steveprozune@hotmail.com>
2021-11-22 16:13:51 +0000

    updates

--- 40-TestKind/05-ModSec_Directory_Int/main.go
+++ 40-TestKind/05-ModSec_Directory_Int/main.go
@@ -9,12 +9,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"time"
-	"unsafe"
 )
 
 func HomeFunc(w http.ResponseWriter, _ *http.Request) {
@@ -31,101 +28,131 @@ func HomeFunc(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func TestFunc(w http.ResponseWriter, r *http.Request) {
-	log.Print("Processing request")
-	urlx, err := url.Parse("https://www.lightbase.io/freeforlife")
+func TestFunc(w http.ResponseWriter, _ *http.Request) {
+
+	log.Print("TestFunc start")
+	log.Println("Directory walk start")
+
+	var files []string
+	root := "/etc/config/"
+	items, _ := ioutil.ReadDir(root)
+	for _, item := range items {
+
+		fileName := root + item.Name()
+		files = append(files, fileName)
+	}
+
+	log.Println("Directory walk -end-")
+
+	log.Println("iterate start")
+	for _, file := range files {
+		fmt.Println("next file = '" + file + "'")
+	}
+	log.Println("iterate -end-")
+
+	log.Println("Call C code start")
+	csize := C.int(len(files))
+	cargs := C.makeCharArray(csize)
+	defer C.freeCharArray(cargs, csize)
+	for i, s := range files {
+		C.setArrayString(cargs, C.CString(s), C.int(i))
+	}
+	C.processArrayString(cargs, csize)
+	log.Println("Call C code -end-")
+
+
+
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data := "Test Func...!!!! X"
+	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
-		log.Println(err)
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(urlx)
-	director := proxy.Director
-	proxy.Director = func(req *http.Request) {
-		director(req)
-		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
-		req.Host = req.URL.Host
-		req.URL.Path = urlx.Path
-	}
-	log.Print("Serving request")
-	proxy.ServeHTTP(w, r)
-}
-
-func InitModSec() {
-	//log.Println("initModSec start")
-	C.MyCInit()
-	//log.Println("initModSec -end-")
-}
-
-func modsec(url, httpMethod, httpProtocol, httpVersion string, clientLink string, clientPort int, serverLink string, serverPort int) int {
-	log.Println("modsec start ", url)
-	Curi := C.CString(url)
-	ChttpMethod := C.CString(httpMethod)
-	ChttpProtocol := C.CString(httpProtocol)
-	ChttpVersion := C.CString(httpVersion)
-	CclientLink := C.CString(clientLink)
-	CclientPort := C.int(clientPort)
-	CserverLink := C.CString(serverLink)
-	CserverPort := C.int(serverPort)
-
-	defer C.free(unsafe.Pointer(Curi))
-	defer C.free(unsafe.Pointer(ChttpMethod))
-	defer C.free(unsafe.Pointer(ChttpProtocol))
-	defer C.free(unsafe.Pointer(ChttpVersion))
-	defer C.free(unsafe.Pointer(CclientLink))
-	defer C.free(unsafe.Pointer(CserverLink))
-
-	start := time.Now()
-	inter := int(C.MyCProcess(Curi, ChttpMethod, ChttpProtocol, ChttpVersion, CclientLink, CclientPort, CserverLink, CserverPort))
-	elapsed := time.Since(start)
-	log.Printf("modsec()=%d, elapsed: %s", inter, elapsed)
-	log.Println("modsec -end-")
-	return inter
-}
-
-func LimitMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("req.URL : \"%s\"", r.URL)
-		log.Printf("Methods : \"%s\"", r.Method)
-
-		uri := r.URL.String()
-		httpMethod := "GET"
-
-		//protocol := "HTTP/1.1"
-		httpProtocol := "HTTP"
-		httpVersion := "1.1"
-
-		//clientSocket := "127.0.0.1:80"
-		clientLink := "127.0.0.1"
-		clientPort := 80
-		//serverSocket := "127.0.0.1:80"
-		serverLink := "127.0.0.1"
-		serverPort := 80
-
-		inter := modsec(uri, httpMethod, httpProtocol, httpVersion, clientLink, clientPort, serverLink, serverPort)
-		if inter > 0 {
-			log.Printf("==== Mod Security Blocked! ====")
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		_, err := fmt.Fprintf(w, "%s", err.Error())
+		if err != nil {
+			log.Fatal(err)
 			return
 		}
-
-		next.ServeHTTP(w, r)
-	})
+	}
 }
 
+//func InitModSec() {
+//	//log.Println("initModSec start")
+//	C.MyCInit()
+//	//log.Println("initModSec -end-")
+//}
+
+//func modsec(url, httpMethod, httpProtocol, httpVersion string, clientLink string, clientPort int, serverLink string, serverPort int) int {
+//	log.Println("modsec start ", url)
+//	Curi := C.CString(url)
+//	ChttpMethod := C.CString(httpMethod)
+//	ChttpProtocol := C.CString(httpProtocol)
+//	ChttpVersion := C.CString(httpVersion)
+//	CclientLink := C.CString(clientLink)
+//	CclientPort := C.int(clientPort)
+//	CserverLink := C.CString(serverLink)
+//	CserverPort := C.int(serverPort)
+//
+//	defer C.free(unsafe.Pointer(Curi))
+//	defer C.free(unsafe.Pointer(ChttpMethod))
+//	defer C.free(unsafe.Pointer(ChttpProtocol))
+//	defer C.free(unsafe.Pointer(ChttpVersion))
+//	defer C.free(unsafe.Pointer(CclientLink))
+//	defer C.free(unsafe.Pointer(CserverLink))
+//
+//	start := time.Now()
+//	inter := int(C.MyCProcess(Curi, ChttpMethod, ChttpProtocol, ChttpVersion, CclientLink, CclientPort, CserverLink, CserverPort))
+//	elapsed := time.Since(start)
+//	log.Printf("modsec()=%d, elapsed: %s", inter, elapsed)
+//	log.Println("modsec -end-")
+//	return inter
+//}
+
+//func LimitMiddleware(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		log.Printf("req.URL : \"%s\"", r.URL)
+//		log.Printf("Methods : \"%s\"", r.Method)
+//
+//		uri := r.URL.String()
+//		httpMethod := "GET"
+//
+//		//protocol := "HTTP/1.1"
+//		httpProtocol := "HTTP"
+//		httpVersion := "1.1"
+//
+//		//clientSocket := "127.0.0.1:80"
+//		clientLink := "127.0.0.1"
+//		clientPort := 80
+//		//serverSocket := "127.0.0.1:80"
+//		serverLink := "127.0.0.1"
+//		serverPort := 80
+//
+//		inter := modsec(uri, httpMethod, httpProtocol, httpVersion, clientLink, clientPort, serverLink, serverPort)
+//		if inter > 0 {
+//			log.Printf("==== Mod Security Blocked! ====")
+//			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+//			return
+//		}
+//
+//		next.ServeHTTP(w, r)
+//	})
+//}
+
 func main() {
-	log.Println("testing")
 	bind := ":3080"
+	log.Println("Start web server on port", bind)
+
+	log.Println("Init routes")
 	gmux := mux.NewRouter()
 	gmux.HandleFunc("/", HomeFunc).Methods("GET")
 	gmux.HandleFunc("/test/artists.php", TestFunc).Methods("GET")
 
-	log.Printf("starting smart reverse proxy on [%s]", bind)
+	//log.Printf("initialize mod sec")
+	//InitModSec()
 
-	log.Printf("initialize mod sec")
-	InitModSec()
-
-	log.Printf("listening [deny]...")
-	if err := http.ListenAndServe(bind, LimitMiddleware(gmux)); err != nil {
+	log.Printf("listening...")
+	//if err := http.ListenAndServe(bind, LimitMiddleware(gmux)); err != nil {
+	if err := http.ListenAndServe(bind, gmux); err != nil {
 		log.Fatalf("unable to start web server: %s", err.Error())
 	}
 }
--- 40-TestKind/05-ModSec_Directory_Int/modsec.c
+++ 40-TestKind/05-ModSec_Directory_Int/modsec.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "modsecurity/modsecurity.h"
 #include "modsecurity/rules_set.h"
 #include "modsecurity/transaction.h"
@@ -7,55 +8,86 @@
 ModSecurity *modsec = NULL;
 RulesSet *rules = NULL;
 
-void MyCInit()
-{
-    //printf("MyCInit start\n");
-    if (modsec != NULL)
-    {
-        return;
-    }
-
-    modsec = msc_init();
-    rules = msc_create_rules_set();
-
-    const char *error = NULL;
-    msc_rules_add_file(rules, "modsecdefault.conf", &error);
-    msc_rules_add_file(rules, "crs-setup.conf", &error);
-    msc_rules_add_file(rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error);
-    if (error != NULL)
-    {
-        printf("msc_rules_add_file error : '%s'\n", error);
-    }
-
-    //msc_rules_dump(rules);
-    //printf("MyCInit -end-\n");
-    return;
+static char **makeCharArray(int size) {
+    fprintf(stderr, "  C makeCharArray %i\n", size);
+    return calloc(sizeof(char*), size);
 }
 
-int MyCProcess(char *uri, char *http_method, char *http_protocol, char *http_version, char *client_link, int client_port, char *server_link, int server_port)
-{
-    Transaction *transaction = NULL;
-    transaction = msc_new_transaction(modsec, rules, NULL);
-
-    fprintf(stderr, "URI='%s'\n", uri);
-    fprintf(stderr, "Method='%s'\n", http_method);
-    fprintf(stderr, "Protocol='%s/%s'\n", http_protocol, http_version);
-    fprintf(stderr, "Client socket='%s:%d'\n", client_link, client_port);
-    fprintf(stderr, "Server socket='%s:%d'\n", server_link, server_port);
-
-    msc_process_connection(transaction, client_link, client_port, server_link, server_port);
-    msc_process_uri(transaction, uri, http_protocol, http_version);
-    msc_process_request_headers(transaction);
-    msc_process_request_body(transaction);
-
-    ModSecurityIntervention intervention;
-    intervention.status = 200;
-    intervention.url = NULL;
-    intervention.log = NULL;
-    intervention.disruptive = 0;
-    int inter = msc_intervention(transaction, &intervention);
-
-    fprintf(stderr, "intervention=%i\n", inter);
-    fprintf(stderr, "inter status=%i\n", intervention.status);
-    return inter;
+static void freeCharArray(char **a, int size) {
+    int i;
+    fprintf(stderr, "  C freeCharArray %d\n", size);
+    for (i = 0; i < size; i++)
+        free(a[i]);
+    free(a);
 }
+
+static void setArrayString(char **a, char *s, int n) {
+    fprintf(stderr, "  C setArrayString [%d] = '%s' start\n", n, s);
+    a[n] = s;
+    fprintf(stderr, "  C setArrayString [%d] = '%s' -end-\n", n, s);
+}
+
+static void processArrayString(char **array, int size) {
+    int i = 0;
+    fprintf(stderr, "  C processArrayString start [%d]\n", i);
+
+    for( i = 0; i < size; i++ )
+    {
+        fprintf(stderr, "  C code file '%s' [%d]\n", ( array[ i ] ), i );
+    }
+
+    fprintf(stderr, "  C processArrayString -end- %i\n", i);
+}
+
+//void MyCInit()
+//{
+//    fprintf(stderr, "MyCInit start\n");
+//    if (modsec != NULL)
+//    {
+//        return;
+//    }
+//
+//    modsec = msc_init();
+//    rules = msc_create_rules_set();
+//
+//    const char *error = NULL;
+//    msc_rules_add_file(rules, "modsecdefault.conf", &error);
+//    msc_rules_add_file(rules, "crs-setup.conf", &error);
+//    msc_rules_add_file(rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error);
+//    if (error != NULL)
+//    {
+//        fprintf(stderr, "msc_rules_add_file error : '%s'\n", error);
+//    }
+//
+//    //msc_rules_dump(rules);
+//    fprintf(stderr, "MyCInit -end-\n");
+//    return;
+//}
+
+//int MyCProcess(char *uri, char *http_method, char *http_protocol, char *http_version, char *client_link, int client_port, char *server_link, int server_port)
+//{
+//    Transaction *transaction = NULL;
+//    transaction = msc_new_transaction(modsec, rules, NULL);
+//
+//    fprintf(stderr, "URI='%s'\n", uri);
+//    fprintf(stderr, "Method='%s'\n", http_method);
+//    fprintf(stderr, "Protocol='%s/%s'\n", http_protocol, http_version);
+//    fprintf(stderr, "Client socket='%s:%d'\n", client_link, client_port);
+//    fprintf(stderr, "Server socket='%s:%d'\n", server_link, server_port);
+//
+//    msc_process_connection(transaction, client_link, client_port, server_link, server_port);
+//    msc_process_uri(transaction, uri, http_protocol, http_version);
+//    msc_process_request_headers(transaction);
+//    msc_process_request_body(transaction);
+//
+//    ModSecurityIntervention intervention;
+//    intervention.status = 200;
+//    intervention.url = NULL;
+//    intervention.log = NULL;
+//    intervention.disruptive = 0;
+//    int inter = msc_intervention(transaction, &intervention);
+//
+//    fprintf(stderr, "intervention=%i\n", inter);
+//    fprintf(stderr, "inter status=%i\n", intervention.status);
+//    return inter;
+//}
