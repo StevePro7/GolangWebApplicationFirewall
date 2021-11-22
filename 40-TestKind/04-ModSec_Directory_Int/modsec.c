1f9a16f52852cd260278880b3ef0053f0245054e
StevePro7 <steveprozune@hotmail.com>
2021-11-22 16:18:47 +0000

    logging

--- 40-TestKind/04-ModSec_Directory_Int/modsec.c
+++ 40-TestKind/04-ModSec_Directory_Int/modsec.c
@@ -2,32 +2,35 @@
 #include <stdlib.h>
 
 static char **makeCharArray(int size) {
-    fprintf(stderr, "  C makeCharArray %i\n", size);
+    fprintf(stderr, "  C makeCharArray\n");
     return calloc(sizeof(char*), size);
 }
 
 static void freeCharArray(char **a, int size) {
     int i;
-    fprintf(stderr, "  C freeCharArray %d\n", size);
+    fprintf(stderr, "  C freeCharArray\n");
     for (i = 0; i < size; i++)
         free(a[i]);
     free(a);
 }
 
 static void setArrayString(char **a, char *s, int n) {
-    fprintf(stderr, "  C setArrayString [%d] = '%s' start\n", n, s);
+    fprintf(stderr, "setArrayString [%d] = '%s' start\n", n, s);
     a[n] = s;
-    fprintf(stderr, "  C setArrayString [%d] = '%s' -end-\n", n, s);
+    fprintf(stderr, "setArrayString [%d] = '%s' -end-\n", n, s);
 }
 
 static void processArrayString(char **array, int size) {
     int i = 0;
+    char *fileName;
     fprintf(stderr, "  C processArrayString start [%d]\n", i);
 
     for( i = 0; i < size; i++ )
     {
-        fprintf(stderr, "  C code file '%s' [%d]\n", ( array[ i ] ), i );
+        fileName = array[ i ] ;
+        //fprintf(stderr, "  C code file '%s' [%d]\n", ( array[ i ] ), i );
+        fprintf(stderr, "  C code file!! '%s'\n", fileName );
     }
 
-    fprintf(stderr, "  C processArrayString -end- %i\n", i);
+    fprintf(stderr, "  C processArrayString -end-\n");
 }
