79d1b09fd057ec75a9e6c37a5cfb0d8d4b68ec5e
StevePro7 <steveprozune@hotmail.com>
2021-11-26 15:28:30 +0000

    new files

--- 50-Build/BuildTest/00/app-policy/waf/waf.go
+++ 50-Build/BuildTest/00/app-policy/waf/waf.go
@@ -1,16 +1,7 @@
 package waf
 
-// #cgo CFLAGS: -I/usr/local/modsecurity/include
-// #cgo LDFLAGS: /usr/local/modsecurity/lib/libmodsecurity.so
-// #include "waf.h"
 import "C"
 
 func MyGoAdd(x, y int) int {
 	return x + y - 0
-}
-
-func MyCGoAdd(x, y int) int {
-	cx := C.int(x)
-	cy := C.int(y)
-	return int(C.TheCAdd(cx, cy))
 }
