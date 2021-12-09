42db7283ce264073dccdb1f13c94dab2af278d75
StevePro7 <steveprozune@hotmail.com>
2021-12-09 17:23:59 +0000

    process test 2+3

--- WAF/01-HelloWorld/waf/waf_test.go
+++ WAF/01-HelloWorld/waf/waf_test.go
@@ -8,6 +8,50 @@ import (
 const directory = "/etc/waf"
 
 // 04.
+func TestProcessHttpRequest_InvalidURL_NoRulesLoad_OK(t *testing.T) {
+
+	filenames := []string{}
+	LoadModSecurityCoreRuleSet(filenames)
+
+	url := "/test/artists.php?artist=0+div+1+union%23foo*%2F*bar%0D%0Aselect%23foo%0D%0A1%2C2%2Ccurrent_user"
+	httpMethod := "GET"
+	httpProtocol := "HTTP"
+	httpVersion := "1.1"
+	clientLink := "http://localhost"
+	clientPort := 80
+	serverLink := "http://localhost"
+	serverPort := 80
+
+	expect := 0
+	actual := ProcessHttpRequest(url, httpMethod, httpProtocol, httpVersion, clientLink, clientPort, serverLink, serverPort)
+
+	if expect != actual {
+		t.Errorf("Expect: %d Actual: %d", expect, actual)
+	}
+}
+
+func TestProcessHttpRequest_InvalidURL_BlockDueToWarning(t *testing.T) {
+
+	filenames := []string{"/etc/waf/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", "/etc/waf/crs-setup.conf", "/etc/waf/modsecdefault.conf"}
+	LoadModSecurityCoreRuleSet(filenames)
+
+	url := "/test/artists.php?artist=0+div+1+union%23foo*%2F*bar%0D%0Aselect%23foo%0D%0A1%2C2%2Ccurrent_user"
+	httpMethod := "GET"
+	httpProtocol := "HTTP"
+	httpVersion := "1.1"
+	clientLink := "http://localhost"
+	clientPort := 80
+	serverLink := "http://localhost"
+	serverPort := 80
+
+	expect := 0
+	actual := ProcessHttpRequest(url, httpMethod, httpProtocol, httpVersion, clientLink, clientPort, serverLink, serverPort)
+
+	if expect != actual {
+		t.Errorf("Expect: %d Actual: %d", expect, actual)
+	}
+}
+
 func TestProcessHttpRequest_ValidURL_OK(t *testing.T) {
 
 	filenames := []string{"/etc/waf/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", "/etc/waf/crs-setup.conf", "/etc/waf/modsecdefault.conf"}
