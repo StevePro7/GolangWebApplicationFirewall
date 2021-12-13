07a063364ccaaae099bada697658c26425d9fb46
StevePro7 <steveprozune@hotmail.com>
2021-12-13 17:41:41 +0000

    pre delete2

--- WAF/05-CgoCallbacks/waf/waf.go
+++ WAF/05-CgoCallbacks/waf/waf.go
@@ -9,3 +9,14 @@ func InitializeModSecurity() {
 	C.InitializeModSecurity()
 	log.Println("GoInitializeModSecurity end")
 }
+
+func Run() {
+	log.Println("GoRUN beg")
+	C.pass_GoAdd()
+	log.Println("GoRUN end")
+}
+
+//export GoAdd
+func GoAdd(a, b int) int {
+	return a + b
+}
--- WAF/05-CgoCallbacks/waf/waf.h
+++ WAF/05-CgoCallbacks/waf/waf.h
@@ -1,7 +1,15 @@
 #include <stdio.h>
+//#include "_cgo_export.h"
 
 void InitializeModSecurity()
 {
     fprintf(stderr, "C InitializeModSecurity beg\n");
     fprintf(stderr, "C InitializeModSecurity end\n");
+}
+
+void pass_GoAdd(void)
+{
+    fprintf(stderr, "C pass_GoAdd beg\n");
+    call_add(&GoAdd);
+    fprintf(stderr, "C pass_GoAdd end\n");
 }
