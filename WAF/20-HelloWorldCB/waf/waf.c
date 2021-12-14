21f40667dcb37ef0c4bf50c9844ecb3507e2d15b
StevePro7 <steveprozune@hotmail.com>
2021-12-14 17:51:37 +0000

    dump out log info

--- WAF/20-HelloWorldCB/waf/waf.c
+++ WAF/20-HelloWorldCB/waf/waf.c
@@ -14,20 +14,20 @@ static void initializeModSecurityImpl();
 
 void call_sgb(texts funcs, char *str)
 {
-    printf("C  call_sgb beg\n");
-    printf("C  call_sgb '%s'\n", str);
+    //printf("C  call_sgb beg\n");
+    //printf("C  call_sgb '%s'\n", str);
     funcs(str);
-    printf("C  call_sgb end\n");
+    //printf("C  call_sgb end\n");
 }
 
 void MyFuncPtr(void *foo, const void *bar)
 {
     char *sgb = (char *)bar;
-    fprintf(stderr, "MyFuncPtr start\n");
+    //fprintf(stderr, "MyFuncPtr start\n");
     //fprintf(stderr, "MyFuncPtr '%s'\n", foo);
-    fprintf(stderr, "MyFuncPtr '%s' [SPLAT..!]\n", sgb);
+    //fprintf(stderr, "MyFuncPtr '%s' [SPLAT..!]\n", sgb);
     call_sgb(&GoText, sgb);
-    fprintf(stderr, "MyFuncPtr -end-..END..!\n");
+    //fprintf(stderr, "MyFuncPtr -end-..END..!\n");
 }
 
 void InitializeModSecurity()
@@ -41,9 +41,9 @@ void InitializeModSecurity()
 static void initializeModSecurityImpl()
 {
     modsec = msc_init();
-    fprintf(stderr, "  C msc_set_log_cb start\n");
+    //fprintf(stderr, "  C msc_set_log_cb start\n");
     msc_set_log_cb(modsec, MyFuncPtr);
-    fprintf(stderr, "  C msc_set_log_cb -end-\n");
+    //fprintf(stderr, "  C msc_set_log_cb -end-\n");
     rules = msc_create_rules_set();
 }
 
@@ -106,21 +106,3 @@ void setArrayString(char **array, char *filename, int index)
 {
     array[index] = filename;
 }
-
-
-//void call_const(ModSecLogCb callback)
-//{
-//    //char *str = "steven";
-//    //const char *buf = "World";
-//    //char *buf = "boland";
-//    printf("C  call_const beg\n");
-//    //callback(str, buf);
-//    //callback(str);
-//    printf("C  call_const end!!\n");
-//}
-// void pass_GoConst(void)
-// {
-//    printf("C  pass_GoConst beg\n");
-//    call_const(&GoConst);
-//    printf("C  pass_GoConst end\n");
-// }
