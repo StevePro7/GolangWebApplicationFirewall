5ffb7f5e36f5daecf5d4802e13996b1fb5c3ce64
StevePro7 <steveprozune@hotmail.com>
2021-12-17 15:42:30 +0000

    implement function

--- WAF/30-LoggingParser/main.go
+++ WAF/30-LoggingParser/main.go
@@ -6,18 +6,22 @@ import (
 )
 
 func main() {
-	fmt.Println("hello")
+	//fmt.Println("hello")
 
 	payload := "ModSecurity: Warning. detected SQLi using libinjection. [file \"/etc/waf/custom-REQUEST-942-APPLICATION-ATTACK-SQLI.conf\"] [line \"45\"] [id \"942100\"] [rev \"\"] [msg \"SQL Injection Attack Detected via libinjection\"] [data \"Matched Data: 1UE1 found within ARGS:artist: 0 div 1 union#foo*/*bar\\x0d\\x0aselect#foo\\x0d\\x0a1,2,current_user\"] [severity \"2\"] [ver \"OWASP_CRS/3.3.2\"] [maturity \"0\"] [accuracy \"0\"] [hostname \"echo-a\"] [uri \"/test/artists.php\"] [unique_id \"163956574873.676390\"] [ref \"v30,53\"]"
-	data := waf.Parser(payload)
-	fmt.Println(data)
+	//data := waf.Parser(payload)
+	//fmt.Println(data)
 
-	dictionary := waf.Parse(payload)
+	dictionary := waf.Parser(payload)
 	fmt.Println(dictionary["file"])
+	fmt.Println(dictionary["line"])
+	fmt.Println(dictionary["id"])
 	fmt.Println(dictionary["msg"])
+	fmt.Println(dictionary["uri"])
+	fmt.Println(dictionary["data"])
 
-	x := 1
-	y := 2
-	res := waf.GoAdd(x, y)
-	fmt.Println(res)
+	//	x := 1
+	//	y := 2
+	//	res := waf.GoAdd(x, y)
+	//	fmt.Println(res)
 }
--- WAF/30-LoggingParser/waf/parser.go
+++ WAF/30-LoggingParser/waf/parser.go
@@ -6,16 +6,12 @@ import (
 	"strings"
 )
 
-func Parse(payload string) map[string]string {
+func Parser(payload string) map[string]string {
 
 	dict := make(map[string]string)
-	dict["file"] = "/tmp"
-	dict["rule"] = "942100"
-	return dict
-}
 
-func Parser(payload string) int {
 	//str1 := "ModSecurity: Warning. detected SQLi using libinjection. [file \"/etc/waf/custom-REQUEST-942-APPLICATION-ATTACK-SQLI.conf\"] [line \"45\"] [id \"942100\"] [rev \"\"] [msg \"SQL Injection Attack Detected via libinjection\"] [data \"Matched Data: 1UE1 found within ARGS:artist: 0 div 1 union#foo*/*bar\\x0d\\x0aselect#foo\\x0d\\x0a1,2,current_user\"] [severity \"2\"] [ver \"OWASP_CRS/3.3.2\"] [maturity \"0\"] [accuracy \"0\"] [hostname \"echo-a\"] [uri \"/test/artists.php\"] [unique_id \"163956574873.676390\"] [ref \"v30,53\"]"
+	//re := regexp.MustCompile(`\[([^\[\]]*)\]`)
 	re := regexp.MustCompile(`\[([^\[\]]*)\]`)
 	//fmt.Printf("Pattern: %v\n", re.String())      // print pattern
 	//fmt.Println("Matched:", re.MatchString(str1)) // true
@@ -25,11 +21,19 @@ func Parser(payload string) int {
 	for _, element := range submatchall {
 		element = strings.Trim(element, "[")
 		element = strings.Trim(element, "]")
-		fmt.Println(element)
+		//fmt.Println(element)
+		data := strings.SplitAfterN(element, " ", 2)
+		leng := len(data)
+		if leng != 2 {
+			fmt.Println("Yikes!")
+		}
+		key := strings.Trim(data[0], " ")
+		val := data[1]
+		//fmt.Println(leng)
+		dict[key] = val
 	}
 
-	return 7
-}
-func GoAdd(x, y int) int {
-	return x + y
+	//dict["file"] = "/tmp"
+	//dict["rule"] = "942100"
+	return dict
 }
