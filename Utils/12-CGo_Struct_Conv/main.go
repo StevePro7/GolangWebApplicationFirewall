370ec28700b81887f5274a9e8b955e93a2f55357
StevePro7 <steveprozune@hotmail.com>
2022-01-25 17:57:33 +0000

    new code2.1

--- Utils/12-CGo_Struct_Conv/main.go
+++ Utils/12-CGo_Struct_Conv/main.go
@@ -3,10 +3,24 @@ package main
 // #include "test.h"
 import "C"
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+type X struct {
+	Y, Z int32
+}
 
 func main() {
 	fmt.Println("beg")
+
+	fmt.Println("beg II")
+	a := &X{Y: 2, Z: 3}
+	s := C.sum(*((*C.struct_x)(unsafe.Pointer(a))))
+	fmt.Println(s)
+	fmt.Println("end II")
+
 	cx := C.int(1)
 	cy := C.int(2)
 	sum := C.int(C.MyCAdd(cx, cy))
--- Utils/12-CGo_Struct_Conv/test.h
+++ Utils/12-CGo_Struct_Conv/test.h
@@ -1,3 +1,14 @@
+struct x
+{
+    int y;
+    int z;
+};
+
+int sum(struct x a)
+{
+    return a.y + a.z;
+}
+
 int MyCAdd(int x, int y)
 {
     return x + y;
