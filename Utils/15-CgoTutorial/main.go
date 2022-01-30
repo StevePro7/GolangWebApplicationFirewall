951cf9486b41ecec21fd8879546252d71657af49
StevePro7 <steveprozune@hotmail.com>
2022-01-30 19:12:19 +0000

    Make sum

--- Utils/15-CgoTutorial/main.go
+++ Utils/15-CgoTutorial/main.go
@@ -16,6 +16,13 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Call to int function with two params
+	res, err := makeSum(5, 4)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Sum of 5 + 4 = %d\n", res)
+
 	fmt.Println("end")
 }
 
@@ -24,6 +31,22 @@ func Hello() error {
 	if err != nil {
 		return errors.New("error calling Hello function: " + err.Error())
 	}
-
 	return nil
+}
+
+// makeSum also is a C binding to make a sum
+// it returns a results and an error
+func makeSum(a, b int) (int, error) {
+	// Convert Go ints to C ints
+	aC := C.int(a)
+	bC := C.int(b)
+
+	sum, err := C.sum(aC, bC)
+	if err != nil {
+		return 0, errors.New("error calling Sum function: " + err.Error())
+	}
+
+	// convert C.int result to Go int
+	res := int(sum)
+	return res, nil
 }
