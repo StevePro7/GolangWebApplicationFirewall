60c961073c0c600c909bd6b233918c889f209104
StevePro7 <steveprozune@hotmail.com>
2022-02-16 09:58:28 +0000

    new project

--- Utils/32-mapstringstring/main.go
+++ Utils/32-mapstringstring/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func unpackDictionary(dictionary map[string]string) ([]string, []string) {
+
+	keys := []string{}
+	values := []string{}
+
+	for key, value := range dictionary {
+		keys = append(keys, key)
+		values = append(values, value)
+	}
+
+	return keys, values
+}
 
 func main() {
 	reqHeaders := make(map[string]string)
+	reqHeaders["key"] = "val"
 	reqHeaders["User-Agent"] = "Microsoft Internet Explorer"
 	reqHeaders["content-type"] = "application/xml"
 
 	fmt.Println(reqHeaders)
+	keys, values := unpackDictionary(reqHeaders)
+	fmt.Println(len(keys))
+	fmt.Println(len(values))
 }
